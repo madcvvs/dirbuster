b04147c727dfb41bebcf5c8242f76de020291f97
Mathias Kujala <cve20146271@gmail.com>
2017-01-22 20:04:49 +0100

    Features added
    The script now exports the results into a text-file.

--- dirbuster.rb
+++ dirbuster.rb
@@ -5,19 +5,26 @@ require "uri"
 
 def browse(website)
 	File.readlines('list.txt').each do |line|
-	
+		file = "results.txt"
 		url = "#{website}/#{line}".chomp
 		res = Net::HTTP.get_response(URI(url))
 		code = res.code
 		
 		if code == "200"
 			print "\033[0;32m#{code} - #{website}#{line}\033[0m"
+			open('results.txt', 'a') { |f|
+				f.puts "#{code} - #{website}#{line}"
+			}
 		elsif code == "404"
 			print "\033[0;31m#{code} - #{website}#{line}\033[0m"
 		else
 			print "\033[0;33m#{code} - #{website}#{line}\033[0m"
+			open('results.txt', 'a') { |f|
+				f.puts "#{code} - #{website}#{line}"
+			}
 		end
 	end
+	puts "Results are saved in \033[0;32m#{file}!\033[0m"
 end
 
 print "Website: "
